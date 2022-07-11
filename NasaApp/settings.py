9b63dfc8d4f12f27cecbab7d18e1f11b84d5e663
alex <alex.goyer@epitech.eu>
2022-07-11 20:59:05 +0200

    Remove django secret key

--- NasaApp/settings.py
+++ NasaApp/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vz5hg9z&o)jn@mv_pn^huc)yds_xmww1m!77$@s43v9h!at-r#'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
